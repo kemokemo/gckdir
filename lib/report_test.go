1fb158b19ff1160d2a23244216b076f4c52e3ae1
kemokemo <t2wonderland@gmail.com>
2018-02-04 16:26:51 +0900

    style: just changed the order of tests.

--- lib/report_test.go
+++ lib/report_test.go
@@ -7,6 +7,35 @@ import (
 	"testing"
 )
 
+func TestCreateReport(t *testing.T) {
+	validResult := HashList{
+		VerifyResult: true,
+		List: []HashData{
+			HashData{RelativePath: "test.bmp", HashValue: "bbbb", VerifyResult: true, Reason: ""},
+		}}
+
+	type args struct {
+		paths  PathList
+		result HashList
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{name: "ValidReport", args: args{result: validResult, paths: PathList{"", ""}}, wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := &bytes.Buffer{}
+			if err := CreateReport(w, tt.args.paths, tt.args.result); (err != nil) != tt.wantErr {
+				t.Errorf("CreateReport() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
 func Test_passFail(t *testing.T) {
 	type args struct {
 		result bool
@@ -44,35 +73,6 @@ func Test_rowAttr(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := rowAttr(tt.args.result); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("rowAttr() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestCreateReport(t *testing.T) {
-	validResult := HashList{
-		VerifyResult: true,
-		List: []HashData{
-			HashData{RelativePath: "test.bmp", HashValue: "bbbb", VerifyResult: true, Reason: ""},
-		}}
-
-	type args struct {
-		paths  PathList
-		result HashList
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{name: "ValidReport", args: args{result: validResult, paths: PathList{"", ""}}, wantErr: false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			w := &bytes.Buffer{}
-			if err := CreateReport(w, tt.args.paths, tt.args.result); (err != nil) != tt.wantErr {
-				t.Errorf("CreateReport() error = %v, wantErr %v", err, tt.wantErr)
-				return
 			}
 		})
 	}
